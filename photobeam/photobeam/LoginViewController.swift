0b08bbb09be8827a8cfed3a949e95d30eca6d85a
mimi schirm <mimischirm@gmail.com>
2020-06-17 21:38:36 -0700

    adds signup and login view

---
+++ photobeam/photobeam/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInViewController.swift
+//  photobeam
+//
+//  Created by Mimi  on 6/17/20.
+//  Copyright © 2020 meems. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
