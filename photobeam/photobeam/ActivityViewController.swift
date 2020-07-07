c306d52fbcc7fff2287807e5cb46b981976376c5
mimi schirm <mimischirm@gmail.com>
2020-07-06 20:05:38 -0700

    sets up segue to tab bar

---
+++ photobeam/photobeam/ActivityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ActivityViewController.swift
+//  photobeam
+//
+//  Created by Mimi  on 7/3/20.
+//  Copyright © 2020 meems. All rights reserved.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ photobeam/photobeam/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  photobeam
+//
+//  Created by Mimi  on 7/3/20.
+//  Copyright © 2020 meems. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
